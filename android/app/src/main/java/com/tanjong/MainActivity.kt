cb6d1d471fbc43eb99e39a6a616e85a847609b84
AnnatarHe <annatar.he+github@gmail.com>
2024-01-19 02:42:31 +0800

    fix(android): update android

--- android/app/src/main/java/com/tanjong/MainActivity.kt
+++ android/app/src/main/java/com/tanjong/MainActivity.kt
@@ -8,30 +8,21 @@ import android.os.Bundle
 import com.zoontek.rnbootsplash.RNBootSplash
 
 class MainActivity : ReactActivity() {
-    @Override
-    protected fun onCreate(savedInstanceState: Bundle?) {
-        RNBootSplash.init(this)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        RNBootSplash.init(this, R.style.BootTheme)
         super.onCreate(null)
     }
 
-    @get:Override
-    protected val mainComponentName: String
-        /**
-         * Returns the name of the main component registered from JavaScript. This is used to schedule
-         * rendering of the component.
-         */
-        protected get() = "tanjong"
+	/**
+	 * Returns the name of the main component registered from JavaScript. This is used to schedule
+	 * rendering of the component.
+	 */
+	override fun getMainComponentName(): String = "tanjong"
 
-    /**
-     * Returns the instance of the [ReactActivityDelegate]. Here we use a util class [ ] which allows you to easily enable Fabric and Concurrent React
-     * (aka React 18) with two boolean flags.
-     */
-    @Override
-    protected fun createReactActivityDelegate(): ReactActivityDelegate {
-        return DefaultReactActivityDelegate(
-            this,
-            mainComponentName,  // If you opted-in for the New Architecture, we enable the Fabric Renderer.
-			fabricEnabled
-        )
-    }
+	/**
+	 * Returns the instance of the [ReactActivityDelegate]. We use [DefaultReactActivityDelegate]
+	 * which allows you to enable New Architecture with a single boolean flags [fabricEnabled]
+	 */
+	override fun createReactActivityDelegate(): ReactActivityDelegate =
+		DefaultReactActivityDelegate(this, mainComponentName, fabricEnabled)
 }

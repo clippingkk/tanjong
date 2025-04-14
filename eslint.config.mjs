7a4b154ba15c0aa548f4a988bd0382c07bd20ab8
AnnatarHe <annatar.he+github@gmail.com>
2025-04-15 03:21:45 +0800

    fix(lint): move to eslint

---
+++ eslint.config.mjs
@@ -0,0 +1,38 @@
+import { defineConfig } from 'eslint/config';
+import typescriptEslint from '@typescript-eslint/eslint-plugin';
+import tsParser from '@typescript-eslint/parser';
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+import js from '@eslint/js';
+import { FlatCompat } from '@eslint/eslintrc';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const compat = new FlatCompat({
+    baseDirectory: __dirname,
+    recommendedConfig: js.configs.recommended,
+    allConfig: js.configs.all,
+});
+
+export default defineConfig([{
+    extends: compat.extends('@react-native'),
+
+    plugins: {
+        '@typescript-eslint': typescriptEslint,
+    },
+
+    languageOptions: {
+        parser: tsParser,
+    },
+}, {
+    files: ['**/*.ts', '**/*.tsx'],
+
+    rules: {
+        '@typescript-eslint/no-shadow': ['error'],
+        'no-shadow': 'off',
+        'no-undef': 'off',
+        'no-comma-dangle': 'off',
+        semi: 'never',
+        'comma-dangle': 'never',
+    },
+}]);

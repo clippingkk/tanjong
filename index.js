d48fc07ef666da0ad53398ebce31b097e69d3500
AnnatarHe <annatar.he+github@gmail.com>
2025-06-11 19:18:40 +0800

    fix(build): disable Sentry and upgrade React Native to 0.79.3

--- index.js
+++ index.js
@@ -8,17 +8,18 @@ import { AppRegistry } from 'react-native'
 import { name as appName } from './app.json'
 import Root from './src/root'
 
-import * as Sentry from '@sentry/react-native'
-import { routingInstrumentation } from './src/utils/sentry'
+// import * as Sentry from '@sentry/react-native'
+// import { routingInstrumentation } from './src/utils/sentry'
 
-Sentry.init({
-  dsn: "https://f06399ea901af86bbc522bf5ba514033@o108564.ingest.sentry.io/4506340814749696",
-  enabled: process.env.NODE_ENV === 'production',
-  integrations: [
-    new Sentry.ReactNativeTracing({
-      routingInstrumentation
-    })
-  ]
-})
+// Sentry.init({
+//   dsn: "https://f06399ea901af86bbc522bf5ba514033@o108564.ingest.sentry.io/4506340814749696",
+//   enabled: process.env.NODE_ENV === 'production',
+//   integrations: [
+//     new Sentry.ReactNativeTracing({
+//       routingInstrumentation
+//     })
+//   ]
+// })
 
-AppRegistry.registerComponent(appName, () => Sentry.wrap(Root))
+// AppRegistry.registerComponent(appName, () => Sentry.wrap(Root))
+AppRegistry.registerComponent(appName, () => Root)
